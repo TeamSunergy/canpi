6ac5ce9e5579e5bd362533da881fa47a917253e2
Matthew Alicea <matthew.ivan.alicea@gmail.com>
2017-11-19 11:14:58 -0500

    Minor updates

--- sim/send.py
+++ sim/send.py
@@ -1,8 +1,23 @@
+#!/usr/sbin/python3.5
+import subprocess
+import can
 # Setup VCAN
-'''
-sudo modprobe vcan
+#subprocess.call(['modprobe vcan'])
 # Create a vcan network interface with a specific name
-sudo ip link add dev vcan0 type vcan
-sudo ip link set vcan0 up
-'''
+#subprocess.call(['ip link add dev vcan0 type vcan'])
+#subprocess.call(['ip link set vcan0 up'])
+
+#can.rc('socketcan', 'vcan0', 128000)
+#bus = can.interface.Bus('socketcan', 'vcan0', 128000)
+bus = can.interface.Bus('vcan0', bustype='virtual')
+
 # Send Messages
+msg = can.Message(arbitration_id=0xc0ffee,
+	data=[0, 25, 0, 1, 3, 1, 4, 1],
+	extended_id=False)
+
+try:
+	bus.send(msg)
+	print("Message sent on {}".format(bus.channel_info))
+except can.CanError:
+	print("ERROR")
