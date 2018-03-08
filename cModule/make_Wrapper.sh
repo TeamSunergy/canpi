22b6d457ec8c6ba942a46e3b5982d7267b6eff2f
Andrew Pobrica <pirit66@gmail.com>
2018-03-08 13:17:44 -0500

    updated c module

--- cModule/make_Wrapper.sh
+++ cModule/make_Wrapper.sh
@@ -1,4 +1,4 @@
 all:
 	gcc -Isrc -fPIC $(pkg-config --cflags --libs python3) -c interpret_CAN.c interpret_CAN_wrap.c
 	gcc -shared -fPIC -o interpret_CAN.so interpret_CAN.o interpret_CAN_wrap.o
-
+	cp interpret_CAN.so ../sim/
