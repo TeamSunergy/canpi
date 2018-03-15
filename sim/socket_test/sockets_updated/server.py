c9e87bfa6ff89748094d35a0c11182606feb45e3
Steve <soaresss@appstate.edu>
2018-03-15 19:13:49 -0400

    reusable address

--- sim/socket_test/sockets_updated/server.py
+++ sim/socket_test/sockets_updated/server.py
@@ -7,6 +7,7 @@ import asyncio
 
 async def echo_server(address, loop, sleep_seconds):
     sock = socket(AF_INET, SOCK_STREAM)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     sock.bind(address)
     sock.listen(1)
     print("Server started. Host: %s Port: %s " % (address[0],address[1]))
@@ -21,10 +22,12 @@ async def echo_server(address, loop, sleep_seconds):
 
 async def echo_handler(client, loop, sleep_seconds):
     while True:
+        json_ex["speed"] += 1
+        json_ex["battery"] += 1
         await asyncio.sleep(sleep_seconds)
         await loop.sock_sendall(client,json.dumps(json_ex).encode())
-        print("Sent user JSON @", datetime.now())
+        #print("Send user JSON @", datetime.now())
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(echo_server(('127.0.0.1',25000), loop, 5))
+    loop.run_until_complete(echo_server(('127.0.0.1',25000), loop, 4))
